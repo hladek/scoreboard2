10ce644d76f494b34054f1dc81fc185493ca7787
Daniel Hladek <daniel.hladek@tuke.sk>
2025-10-14 12:17:16 +0200

    api methods

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -35,5 +35,64 @@ export default {
   // User endpoints
   getUsers() {
     return apiClient.get('/api/users');
+  },
+
+  // Location endpoints
+  createLocation(name, date) {
+    return apiClient.post('/api/locations', { name, date });
+  },
+
+  getLocations() {
+    return apiClient.get('/api/locations');
+  },
+
+  getLocationById(id) {
+    return apiClient.get(`/api/locations/${id}`);
+  },
+
+  getContestsByLocationId(id) {
+    return apiClient.get(`/api/locations/${id}/contests`);
+  },
+
+  getTeamsByLocationId(id) {
+    return apiClient.get(`/api/locations/${id}/teams`);
+  },
+
+  updateLocation(id, name) {
+    return apiClient.put(`/api/locations/${id}`, { name });
+  },
+
+  // Team endpoints
+  createTeam(name, affiliation, location_id) {
+    return apiClient.post('/api/teams', { name, affiliation, location_id });
+  },
+
+  getTeams() {
+    return apiClient.get('/api/teams');
+  },
+
+  getTeamById(id) {
+    return apiClient.get(`/api/teams/${id}`);
+  },
+
+  updateTeam(id, name) {
+    return apiClient.put(`/api/teams/${id}`, { name });
+  },
+
+  // Contest endpoints
+  createContest(name, description, max_rounds, location_id) {
+    return apiClient.post('/api/contests', { name, description, max_rounds, location_id });
+  },
+
+  getContests() {
+    return apiClient.get('/api/contests');
+  },
+
+  getContestById(id) {
+    return apiClient.get(`/api/contests/${id}`);
+  },
+
+  updateContest(id, name) {
+    return apiClient.put(`/api/contests/${id}`, { name });
   }
 };
